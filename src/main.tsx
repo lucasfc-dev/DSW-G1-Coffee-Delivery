0aaf62965bd585bb8e56657a6446e2afcf2b230d
lucasfc-dev <lucasfrancocastro@gmail.com>
2025-04-25 20:59:08 -0300

    MM

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Cart } from './pages/Cart'
 import { Home } from './pages/Home'
 import { Success } from './pages/Success'
 import { App } from './App'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-      {
-        path: '/order/:orderId/success',
-        element: <Success />,
-      },
-    ],
-  },
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <Home />,
+//       },
+//       {
+//         path: '/cart',
+//         element: <Cart />,
+//       },
+//       {
+//         path: '/order/:orderId/success',
+//         element: <Success />,
+//       },
+//     ],
+//   },
+// ])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+        <Route path='/order/:orderId/success' element={<Success />}></Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
+
